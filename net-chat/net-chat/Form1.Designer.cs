41ec95b43e8910152284564c1c11ebf9961fde3a
cybex-dev <s213473135@nmmu.ac.za>
2017-11-30 15:40:52 +0200

    Added sources

---
+++ net-chat/net-chat/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace net_chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.inputT_Msg = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chatPanel1 = new ChatPanel();
+            this.SuspendLayout();
+            // 
+            // inputT_Msg
+            // 
+            this.inputT_Msg.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.inputT_Msg.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.HistoryList;
+            this.inputT_Msg.Location = new System.Drawing.Point(0, 296);
+            this.inputT_Msg.Multiline = true;
+            this.inputT_Msg.Name = "inputT_Msg";
+            this.inputT_Msg.Size = new System.Drawing.Size(406, 27);
+            this.inputT_Msg.TabIndex = 11;
+            this.inputT_Msg.Text = "Enter message to send";
+            this.inputT_Msg.TextChanged += new System.EventHandler(this.inputT_Msg_TextChanged);
+            this.inputT_Msg.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inputT_Msg_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.Location = new System.Drawing.Point(438, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 27);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "SEND";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.ShowHelp = true;
+            this.openFileDialog1.SupportMultiDottedExtensions = true;
+            this.openFileDialog1.Title = "Select file(s) to send using [Ctrl + click]";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(409, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 27);
+            this.button1.TabIndex = 15;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chatPanel1
+            // 
+            this.chatPanel1.AutoScroll = true;
+            this.chatPanel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.chatPanel1.Location = new System.Drawing.Point(0, 0);
+            this.chatPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.chatPanel1.Name = "chatPanel1";
+            this.chatPanel1.Size = new System.Drawing.Size(507, 293);
+            this.chatPanel1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 322);
+            this.Controls.Add(this.chatPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.inputT_Msg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox inputT_Msg;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private ChatPanel chatPanel1;
+    }
+}
+
---
+++ net-chat/net-chat/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.IO;
+using System.Net.NetworkInformation;
+
+namespace net_chat
+{
+    public partial class Form1 : Form
+    {
+        static Form1 frm1;
+        static IPAddress _IPADDRESS;
+        static IPAddress _MultiIP;
+        const int _PORT = 80;
+        static Thread T;
+        static bool bStop = false;
+        //static List<string> _ipaddress_list = new List<string>();
+        static string[] _files_to_send;
+
+        public Form1()
+        {
+            InitializeComponent();
+            frm1 = this;
+        }
+
+        public static IPAddress GetDefaultGateway()
+        {
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                var card = item;
+                if (card != null && card.Speed != -1 && item.OperationalStatus != OperationalStatus.Down && card.NetworkInterfaceType != NetworkInterfaceType.Loopback && card.NetworkInterfaceType != NetworkInterfaceType.Tunnel)
+                {
+                    UnicastIPAddressInformationCollection x = item.GetIPProperties().UnicastAddresses;
+                    foreach (IPAddressInformation ipitem in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ipitem.IsDnsEligible)
+                            return ipitem.Address;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void sendText(string _message)
+        {
+            sendMessage(_message, null, null);
+        }
+
+        //private void sendMessage(string _MESSAGE, Image img, string[] files)
+        //{
+
+        //    //handle image and files
+        //    Socket _listener_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        //    IPAddress localip = _MultiIP;
+        //    _listener_socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(localip));
+        //    _listener_socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 1);
+        //    IPEndPoint ipep = new IPEndPoint(localip, _PORT);
+        //    _listener_socket.Connect(ipep);
+        //    //send data to multicast group
+        //    string message_to_send = _IPADDRESS.ToString() + "~m~" + _MESSAGE + "\n";
+        //    byte[] bytes = new byte[message_to_send.Length * sizeof(char)];
+        //    System.Buffer.BlockCopy(message_to_send.ToCharArray(), 0, bytes, 0, bytes.Length);
+        //    _listener_socket.Send(bytes, bytes.Length, SocketFlags.None);
+
+        //    _listener_socket.Close();
+        //}
+
+        private void sendMessage(string _MESSAGE, Image img, string[] files)
+        {
+            Socket _listener_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            foreach (IPAddress localIP in Dns.GetHostAddresses(Dns.GetHostName()).Where(i => i.AddressFamily == AddressFamily.InterNetwork))
+            {
+                //handle image and files                
+                _listener_socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(_MultiIP/*, localIP*/));
+                _listener_socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 1);
+                //_listener_socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                //_listener_socket.MulticastLoopback = true;
+                _listener_socket.Connect(new IPEndPoint(_MultiIP, _PORT));
+                //send data to multicast group
+                string message_to_send = _IPADDRESS.ToString() + "~m~" + _MESSAGE + "\n";
+                byte[] bytes = new byte[message_to_send.Length * sizeof(char)];
+                System.Buffer.BlockCopy(message_to_send.ToCharArray(), 0, bytes, 0, bytes.Length);
+                _listener_socket.Send(bytes, bytes.Length, SocketFlags.None);
+            }
+            _listener_socket.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //handle images and file
+            //if (true)
+            //{
+            sendText(inputT_Msg.Text);
+            //}
+            inputT_Msg.Text = "Enter message to send";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _IPADDRESS = GetDefaultGateway();
+            _MultiIP = IPAddress.Parse("224.0.0.3");
+            Form1 frm1 = this;
+            frm1.MaximizeBox = false;
+            frm1.MinimizeBox = false;
+            T = new Thread(new ThreadStart(recieveText));
+            T.IsBackground = true;
+            T.Start();
+        }
+
+        private void recieveText()
+        {
+            //initialise multicast group and bind to interface
+            Socket _sender_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, _PORT);
+            _sender_socket.Bind(ipep);
+            IPAddress localip = _MultiIP;
+            _sender_socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(localip, IPAddress.Any));
+
+            //recieve data to multicast group
+            //while (true)
+            //{
+            while (_sender_socket.IsBound && !bStop)
+            {
+                byte[] b = new byte[1024];
+                _sender_socket.Receive(b);
+                char[] chars = new char[b.Length / sizeof(char)];
+                System.Buffer.BlockCopy(b, 0, chars, 0, b.Length);
+
+                string _message = new string(chars).Trim();
+                string ip = _message.Substring(0, _message.IndexOf("~"));
+                _message = _message.Remove(0, _message.IndexOf("~") + 1);
+                string _flag = _message.Substring(0, 1);
+                _message = _message.Remove(0, _message.IndexOf("~") + 1);
+
+                _message = _message.Replace("\0", string.Empty);
+
+                handleData(ip, _flag, _message);
+            }
+        }
+
+        private void handleData(string ip, string flag, string message/*, Image img, string[] files*/)
+        {
+            ChatPanel.position pos = ChatPanel.position.right;
+            if (ip != _IPADDRESS.ToString())
+            {
+                switch (flag)
+                {
+                    case "m":
+                        pos = ChatPanel.position.left;
+                        break;
+
+                    case "f":
+                    default:
+                        break;
+                }
+            }
+            AddTextToChat(ip, message, pos);
+        }
+
+        void AddTextToChat(string ip, string message, ChatPanel.position pos)
+        {
+            chatPanel1.BeginInvoke((MethodInvoker)delegate () { chatPanel1.AddMessage(pos, ip, message); });
+        }
+
+        private void inputT_Msg_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (inputT_Msg.Text == "")
+                inputT_Msg.Text = "Enter message to send";
+            else
+                if (inputT_Msg.Text == "Enter message to send")
+                inputT_Msg.Text = "";
+            if (e.KeyCode == Keys.Return)
+            {
+                sendText(inputT_Msg.Text);
+                inputT_Msg.Text = "Enter message to send";
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            _files_to_send = openFileDialog1.FileNames;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dlgResult = openFileDialog1.ShowDialog();
+        }
+
+        private void inputT_Msg_TextChanged(object sender, EventArgs e)
+        {
+            //Size = TextRenderer.MeasureText(_message, this.Font, new Size(this.Width, 0), TextFormatFlags.WordBreak),
+        }
+    }
+}
